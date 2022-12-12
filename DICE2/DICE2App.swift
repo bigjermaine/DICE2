b4a1b7691473815fd82c0a5e0048b5f6098ec303
daniel jermaine <danieljermaine2@gmail.com>
2022-12-12 11:40:30 +0100

    Initial Commit

---
+++ DICE2/DICE2App.swift
@@ -0,0 +1,17 @@
+//
+//  DICE2App.swift
+//  DICE2
+//
+//  Created by Apple on 12/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct DICE2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
